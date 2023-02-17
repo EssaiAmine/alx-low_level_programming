ff3d5695b732346486d2552627db280825e21e5b
Andromeda <Essaifi Amine>
2023-02-17 15:17:55 +0100

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include<stdio.h>
 
 /**
-* main - a function tjat generates a num and prints (positive, negative or zero).
+* main - function  generates a num and prints (positive, negative or zero).
 *
 * Retrun: Always 0 (Success)
 */
