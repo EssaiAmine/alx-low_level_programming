3ef1747cc6f1d66d87509920037cefa2a0f76ce0
Andromeda <Essaifi Amine>
2023-02-21 16:54:59 +0100

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,9 +3,8 @@
 
 /**
  * main - print  _putchar
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * 
+ * 0_putchar - writes the character c to stdout
+ * @c: The character to print 
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
