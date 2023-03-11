8981c318f2be59418c6b13b19623489c54bc3983
Andromeda <Essaifi Amine>
2023-03-11 15:13:22 +0100

    libmy.a

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: the string to be printed
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+    /* function implementation */
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
+}
+
